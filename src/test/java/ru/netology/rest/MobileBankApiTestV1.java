34e45fbd4d9519616a3cd4b8594c1e16322c58f8
mac <dmitri-shadri@yandex.ru>
2021-10-26 23:15:45 +0300

    clean test

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -20,7 +20,7 @@ class MobileBankApiTestV1 {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body(matchesJsonSchemaInClasspath("accounts.schema.id"))
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
 
                 ;
     }
